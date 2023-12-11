686ff1b07355df19daad8743ab5727d76a1c916d
mawir157 <jamesmthompson2@gmail.com>
2023-12-11 19:28:59 +0000

    GOlaxy

---
+++ Go/Day11.go
@@ -0,0 +1,63 @@
+package main
+
+import AH "./adventhelper"
+
+import (
+)
+
+type Pos struct {
+	r, c int
+}
+
+func parseInput(ss []string, ex int) (ret int) {
+	ret = 0
+	row_offset := 0
+	empty_cols := []int{}
+
+	for ic := 0; ic < len(ss[0]); ic++ {
+		occ := false
+		for ir := 0; ir < len(ss); ir++ {
+
+			if AH.RuneAt(ss[ir], ic) == '#' {
+				occ = true
+				break
+			}
+		}
+		if !occ {
+			empty_cols = append(empty_cols, ic)
+		}
+	}
+
+	ps := []Pos{}
+	for ir := 0; ir < len(ss); ir++ {
+		s := ss[ir]
+		if !AH.ContainsChar(s, '#') {
+			row_offset += ex
+		}
+		col_offset := 0
+		for ic := 0; ic < len(s); ic++ {
+			if AH.ContainsInt(empty_cols, ic) {
+				col_offset += ex
+			}
+			if AH.RuneAt(ss[ir], ic) == '#' {
+				ps = append(ps, Pos{ir + row_offset, ic + col_offset})
+			}
+		}
+	}
+
+	for i := 0; i < len(ps); i++ {
+		for j := i + 1; j < len(ps); j++ {
+			ret += AH.AbsInt(ps[i].r - ps[j].r) + AH.AbsInt(ps[i].c - ps[j].c)
+		}
+	}
+
+	return
+} 
+
+func main() {
+	ls, _ := AH.ReadStrFile("../input/input11.txt")
+
+	AH.PrintSoln(11, parseInput(ls, 1), parseInput(ls, 999999))
+
+	return
+}
