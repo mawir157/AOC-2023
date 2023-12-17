6c1f8d9c9ab3d7ab1ea937f0f78ca34a9da8e44d
mawir157 <jamesmthompson2@gmail.com>
2023-12-17 16:05:21 +0000

    speed up compile times...

--- C++/includes.h
+++ C++/includes.h
@@ -1,21 +1,21 @@
 #pragma once
 
 #include <algorithm>
-#include <cmath>
-#include <deque>
+// #include <cmath>
+// #include <deque>
 #include <iostream>
 #include <fstream>
-#include <functional>
+// #include <functional>
 #include <list>
 #include <map>
-#include <memory>
+// #include <memory>
 #include <numeric>
-#include <regex>
+// #include <regex>
 #include <string>
 #include <sstream>
 #include <set>
 #include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <utility>
 #include <vector>
