4ed4e40be249c879cdf981e8ab1532507b7685b2
mawir157 <jamesmthompson2@gmail.com>
2023-12-21 21:31:06 +0000

    [21] Tidy up

--- C++/Day21.cpp
+++ C++/Day21.cpp
@@ -61,16 +61,16 @@ namespace Day21
 		return ns;
 	}
 
-	std::map<Pos, int> spread(const std::map<Pos, int> cur, const Grid & grid)
+	std::set<Pos> spread(const std::set<Pos> cur, const Grid & grid)
 	{
 		const int rlim = grid.size() - 1;
 		const int clim = grid[0].size() - 1;
-		std::map<Pos, int> next;
-		for (auto [p, v] : cur) {
+		std::set<Pos> next;
+		for (auto p : cur) {
 			const auto ns = nbrs(p, rlim, clim);
 			for (auto n :  ns) {
 				if (grid[n.r][n.c] == 0) {
-					next[n] += 1;
+					next.insert(n);
 				}
 			}
 		}
@@ -81,19 +81,19 @@ namespace Day21
 	// Eventually the grid flips between two stable state
 	// magic[0]/[1] are the number of live cells in the whole grid in these states
 	// magic[2]/[3] are the number of live cells in the CORNERS we will use this 
-	// later. FUCK YOU ERIC
-	std::vector<int> getMagicNumbers(const Grid & grid, const Pos start)
+	// later.
+	std::vector<int> getMagicNumbers(const Grid & grid, std::set<Pos> & cur)
 	{
-		std::map<Pos, int> cur = { {start, 1} };
-		// do 1000 iterations to get to a stable state
-		for (int i = 0; i < 1000; ++i) {
+		// std::map<Pos, int> cur = { {start, 1} };
+		// do enough iterations to get to a stable state 
+		// we've lready done 64 so another 66 is enough (130=64+66 and even)
+		for (int i = 0; i < 66; ++i) {
 			cur = spread(cur, grid);
 		}
 
-
 		std::vector<int> stable{0, 0, 0, 0};
-		for (int r = 0; r <  grid.size(); ++r) {
-			for (int c = 0; c < grid[r].size(); ++c) {
+		for (int r = 0; r <  (int)grid.size(); ++r) {
+			for (int c = 0; c < (int)grid[r].size(); ++c) {
 				Pos p(r, c);
 				if (cur.count(p) != 0) {
 					const int absV = std::abs(r-65) + std::abs(c-65);
@@ -108,8 +108,8 @@ namespace Day21
 		}
 
 		cur = spread(cur, grid);
-		for (int r = 0; r <  grid.size(); ++r) {
-			for (int c = 0; c < grid[r].size(); ++c) {
+		for (int r = 0; r < (int)grid.size(); ++r) {
+			for (int c = 0; c < (int)grid[r].size(); ++c) {
 				Pos p(r, c);
 				if (cur.count(p) != 0) {
 					const int absV = std::abs(r-65) + std::abs(c-65);
@@ -131,31 +131,27 @@ namespace Day21
 		const auto ls = AH::ReadTextFile(filename);
 		Pos start(0,0);
 		const auto grid = parseInput(ls, start);
-		printf("Old Grid Dims = (%lld, %lld)\n", grid.size(), grid[0].size());
-		std::map<Pos, int> cur = { {start, 1} };
-		std::map<std::pair<int, int>, bool> metagrid;
+		std::set<Pos> cur = { start };
 
 		for (int i = 0; i < 64; ++i) {
 			cur = spread(cur, grid);
 		}
+		const int part1 = cur.size();
 
-		const auto magic = getMagicNumbers(grid, start);
-		const auto bigN = 26501365; // = INTEGER * grid dimn + 1/2 grid dimn FUCK YOU ERIC
-		// printf("(%d, %d)|(%d,%d)\n", magic[0], magic[1], magic[2], magic[3]);
-		// ((n+1)*(n*1)) * odd_full + (n*n) * even_full - (n+1) * odd_corners + n * even_corners;
-		// How many full grids have walked out covered? This is exactly an integer.
+		const auto magic = getMagicNumbers(grid, cur);
+		const auto bigN = 26501365; // = INTEGER * grid dimn + 1/2 grid dimn
+		// How many full grids have walked? This is exactly an integer.
 		const int64_t n = ((bigN - (grid.size() / 2)) / grid.size());
 		// draw a picture we have (n+1)^2 Odd parity stable states + n^2 Even parity
 		// states then need to remove n+1 Odd Corners and add n Even Corners
-		// FUCK YOU ERIC
 		// The grid has very specific properties that means this works - the row and
 		// column containing the start are empty and there is an empty region around
 		// the corner diagonals.
 		// THIS WILL NOT WORK FOR A GENERIC GRID OR A NON INTEGER MULTIPLE FOR
-		// NUMBER OF STEPS - ONE FINAL TIME - FUCK YOU ERIC.
+		// NUMBER OF STEPS
 		const int64_t part2 = ((n+1)*(n+1)*magic[1]) + (n*n*magic[0]) - ((n+1)*magic[3]) + (n*magic[2]);
 
-		AH::PrintSoln(21, cur.size(), part2);
+		AH::PrintSoln(21, part1, part2);
 
 		return 0;
 	}
